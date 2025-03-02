462d3e539fe59bdb4751343380f5a388e5471580
Jerbesson Silva <jerbessonc@gmail.com>
2025-03-02 20:57:28 -0300

    adiciona modelos de Projeto, Tag e Contato; atualiza admin e views; cria template home

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Project, Tag, Contact
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project_type', 'created_at')
+    list_filter = ('project_type', 'tags')
+    search_fields = ('title', 'description')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    readonly_fields = ('created_at',)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Project, Tag
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+def projects(request):
+    projects = Project.objects.all().order_by('-created_at')
+    tags = Tag.objects.all()
+    return render(request, 'main/projects.html', {
+        'projects': projects,
+        'tags': tags
+    })
+
+def contact(request):
+    return render(request, 'main/contact.html')
